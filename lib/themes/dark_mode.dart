0409c764ff000bfe9720d85fbb6e076827186cc9
Duks31 <chidubemjan31@gmail.com>
2024-04-22 12:00:19 +0100

    modeified dark theme

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -3,27 +3,9 @@ import "package:flutter/material.dart";
 ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
     background: const Color.fromARGB(255, 20, 20, 20), 
-    primary: const Color.fromARGB(255, 122, 122, 122), 
-    secondary: const Color.fromARGB(255, 30, 30, 30), 
-    tertiary: const Color.fromARGB(255, 47, 47, 47),
-    inversePrimary: Colors.grey.shade700,
+    primary: Colors.green.shade900, 
+    secondary: Colors.green.shade400, 
+    tertiary: Colors.white,
+    inversePrimary: Colors.green.shade900,
   ),
 );
-
-
-// import 'package:flutter/material.dart';
-
-// ThemeData darkMode = ThemeData(
-//   brightness: Brightness.dark,
-//   colorScheme: ColorScheme.dark(
-//     background: Colors.grey.shade900, 
-//     primary: Colors.green.shade400, 
-//     secondary: Colors.green.shade600, 
-//     tertiary: Colors.grey.shade800,
-//     surface: Colors.grey.shade700,
-//     onBackground: Colors.white,
-//     onPrimary: Colors.black,
-//     onSecondary: Colors.black,
-//     onSurface: Colors.white,
-//   ),
-// );
